369ffd189a3d6417b996d83019b88cc443d5e572
JigyashuGupta <jigyashuguptaa@gmail.com>
2022-07-01 23:16:59 +0530

    made UI looking better than before

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kBodyTextTheme = TextTheme(bodyText2: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,),);
+const kTextFieldStyle = TextStyle(fontSize: 20);
